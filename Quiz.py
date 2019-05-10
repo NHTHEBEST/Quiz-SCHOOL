fa5a9415e883c60003b78d1ba2de14e708b5e2ee
Noam Hadar <nhthebest.noam.hadar@gmail.com>
2019-05-10 13:12:55 +1200

    add gen.py aka genarate funtion

--- Quiz.py
+++ Quiz.py
@@ -16,3 +16,22 @@ class question:
         return "Question : "+self.text+"\nAnswer : "+str(self.answer)
 
 
+    def Genarate(level): # genarate func
+        ran = random.randint(1,4) # add sub mul div
+        q = ""
+        a = 0
+        q1 = random.randint(0,100) * level # first num
+        q2 = random.randint(0,100) * level # sec num
+        if ran == 1: # add
+                q = "%d + %d = "%(q1,q2) # text
+                a = q1+q2 # awser
+        elif ran == 2: # sub
+                q = "%d - %d = "%(q1,q2)
+                a = q1-q2
+        elif ran == 3: # mul
+                q = "%d x %d = "%(q1,q2)
+                a = q1*q2
+        else:          # div
+                q = "%d / %d = "%(q1,q2)
+                a = q1/q2
+        return question(q, a) # new class 
